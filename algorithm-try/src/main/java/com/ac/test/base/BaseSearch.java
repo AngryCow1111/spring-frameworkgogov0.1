cf802656ac84548a8ba4d90a3c6b4c1caf95d9be
angrycow1111 <angrycow1111@gmail.com>
2020-08-23 13:24:38 +0800

    update

--- algorithm-try/src/main/java/com/ac/test/base/BaseSearch.java
+++ algorithm-try/src/main/java/com/ac/test/base/BaseSearch.java
@@ -3,13 +3,26 @@ package com.ac.test.base;
 public class BaseSearch {
   public static int binarySearch(int[] sourceData, int searchKey) {
     int low = 0;
-    int high = sourceData.length;
+    int high = sourceData.length - 1;
+    int mid = -1;
+    int guess = Integer.MIN_VALUE;
     while (low < high) {
-      int mid = (low + high) / 2;
-      if (searchKey == sourceData[mid]) {
+      mid = (low + high) / 2;
+      guess = sourceData[mid];
+      if (searchKey == guess) {
         return mid;
       }
-      if (searchKey > mid) {}
+      if (searchKey > guess) {
+        low = mid + 1;
+        continue;
+      }
+      high = mid + 1;
     }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    int search = binarySearch(new int[] {1, 3, 5, 9}, 1);
+    System.out.println(search);
   }
 }
